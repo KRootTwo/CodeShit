504eacc5eea834ff959786e50a0d8a1eefe1bf5d
KTV <koustavdns289@gmail.com>
2026-02-12 00:06:01 +0530

    Porshe go vroom

--- Java/OOP1/src/gettinStarted/Car.java
+++ Java/OOP1/src/gettinStarted/Car.java
@@ -1,11 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Car {
 	private final String name;
 	private final String model;
 	private final int year; 
 	private String engine;
 	private final float maxSpeed;
-	private final boolean forSale;
-	private final Color color; 
+	private boolean isDrivable;
+	private final Color color;
+	private float currentFuel;
+	private boolean isRunnin = false;
 	
 	public enum Color {
 		White, Arctic_Grey, Pyro_Red, Black, Guards_Red, 
@@ -14,28 +19,97 @@ public class Car {
 	}
 	
 	Car(String name, String model, String engine, int year, 
-		Color color, float maxSpeed, boolean forSale) {
+		Color color, float maxSpeed) {
 		this.name = name;
 		this.model = model;
 		this.engine = engine;
 		this.year = year;
 		this.color = color;
 		this.maxSpeed = maxSpeed;
-		this.forSale = forSale;
+		loadFuel();
 	}
-	void showDs() {
+	public void showDs() {
 		System.out.println("Name: " + name);
 		System.out.println("Model: " + model);
 		System.out.println("Engine: : " + engine);
 		System.out.println("Year: " + year);
 		System.out.println("Color: " + color);
 		System.out.println("Max Speed: " + (int) maxSpeed + " Km/h");
-		System.out.println("forSale: " + forSale);
+		System.out.println("isDrivable: " + isDrivable);
+	}
+	public void addFuel(float n) {
+		currentFuel += n;
+		saveFuel();
 	}
     public void swapEngine(String s) {
         engine = s;
     }
-	public String getName() {
+	public void chkbrkdwn() {
+		int x = (int) ((Math.random() * 9) + 1);
+		if (x == 5) {
+			System.out.println("Brrrr...voooshh. Think there's a breakdown mate.");
+			isDrivable = false;
+			isRunnin = false;
+		}
+	}
+	public void saveFuel() {
+		try {
+			FileWriter fw = new FileWriter("fuel.txt");
+			fw.write(String.valueOf(currentFuel));
+			fw.close();
+		} catch (IOException e) {
+			System.out.println("Error saving file.");
+		}
+	}
+	public void loadFuel() {
+		try {
+			File f = new File("fuel.txt");
+			if (f.exists()) {
+				Scanner blehh = new Scanner(f);
+				currentFuel = Float.parseFloat(blehh.nextLine());
+				blehh.close();
+			}
+		} catch (IOException e) {
+			System.out.println("Error loading fuel");
+		}
+	}
+	public Car fix() {
+		isDrivable = true;
+		return this;
+	}
+	public Car start() {
+		if (isDrivable) {
+		chkbrkdwn();
+		}
+		if (isDrivable) {
+			if (currentFuel <= 0) System.out.println("No fuel, mate.");
+			else if (currentFuel < 5) System.out.println("Low fuel. In reserved mode. Add fuel");
+			else {
+				System.out.println("Injecting fuel... Starting engine...");
+				if (isDrivable) {
+					isRunnin = true;
+					System.out.println("Car Started.");
+				}
+			}
+		} else {
+			System.out.println("Car is not drivable. Fix it first.");
+		}
+		return this;
+	}
+	public Car drive() {
+		if (isRunnin) {
+			System.out.println("Enjoy the ride blud...");
+			currentFuel -= 2;
+		} else {
+			System.out.println("Start the car first.");
+		}
+		saveFuel();
+		return this;
+	}
+}
+
+/**
+ * 	public String getName() {
 		return name;
 	}
 	public String getModel() {
@@ -50,10 +124,10 @@ public class Car {
 	public float getMaxSpeed() {
 		return maxSpeed;
 	}
-	public boolean isforSale() {
-		return forSale;
+	public boolean isisDrivable() {
+		return isDrivable;
 	}
 	public Color getColor() {
 		return color;
 	}
-}
+ */
--- Java/OOP1/src/gettinStarted/Main.java
+++ Java/OOP1/src/gettinStarted/Main.java
@@ -2,10 +2,14 @@
 public class Main {
     public static void main(String[] args) {
 		Car c1 = new Car("Porshe", "911 GT3 RS", 
-				"NA I6", 2022, Car.Color.Arctic_Grey, 296, true);
-        c1.showDs();
-        c1.swapEngine("Turbo charged V-8");
-        c1.showDs();
+				"NA I6", 2022, Car.Color.Pyro_Red, 296);
+        // c1.showDs();
+        // c1.swapEngine("Turbo charged V-8");
+        // c1.showDs();
+
+        // c1.addFuel(67);
+        // c1.fix();
+        c1.start().drive();
     }
 }
 
