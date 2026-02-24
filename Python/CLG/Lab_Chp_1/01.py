120d63cd7a2475ffed65f6e3de4d7a92cff18f53
KTV <koustavdns289@gmail.com>
2026-02-24 18:30:19 +0530

    lab_chp_1 2

---
+++ Python/CLG/Lab_Chp_1/01.py
@@ -0,0 +1,35 @@
+'''
+# 1. Sets and Set-Builder Notation
+# Note: All input should be user-defined
+# 1. Write a program to input two finite sets A and B and compute:
+# • A ∪ B
+# • A ∩ B
+# • A − B
+# • B − A
+# 2. Given a universal set U and a set A, write a program to find the complement of A.
+# 3. Write a program to generate a set using set-builder notation:
+# A = {x ∈ N | x ≤ 20 and is even}.
+# 4. Write a program to verify whether a given set is an empty set, singleton set, or
+# finite set.
+'''
+
+# 1
+
+# A = set(map(int, input("Enter numero: ").split()))
+# B = set(map(int, input("Enter numero: ").split()))
+
+A = {1, 2, 3, 4, 5, 6, 7}
+B = {4, 5, 6, 7, 8, 9, 0}
+
+AunionB = set(i for n in [A, B] for i in n)
+AintsecB = set(i for i in A if i in B)
+AdiffB = set(i for i in A if i not in B)
+BdiffA = set(i for i in B if i not in A)
+
+print(AunionB)
+print(AintsecB)
+print(AdiffB)
+print(BdiffA)
+
+# 2
+
